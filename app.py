41b739e834eaaef58f7573863ae19ce0fc6a94ed
dravos <dagmawi.sraj@gmail.com>
2018-01-17 12:52:22 -0500

    Extend flask to return personal html

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+@app.route('/ds3532')
+def me()
+	return render_template('me.html')
 
 
 if __name__ == '__main__':
